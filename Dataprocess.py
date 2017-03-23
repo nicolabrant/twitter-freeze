9045b34df9439ab4bc4e3807352ecbdf21cd49ed
bobodybibble <bobodybob@gmail.com>
2017-03-23 19:35:35 +0000

    Add files via upload

--- Dataprocess.py
+++ Dataprocess.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 import numpy as np
 import matplotlib as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from pylab import *
 
 tweets = []
 
@@ -31,64 +32,67 @@ pp = PdfPages('analysis.pdf')
 tzresult = Counter(timezone)
 plottz = tzresult.items()
 labelstz, sizestz = zip(*plottz)
-arrysizestz= numpy.array(sizestz)
-totaltz=numpy.array(sum(Decimal(i) for i in sizestz))
+arrysizestz= np.array(sizestz)
+totaltz=np.array(sum(Decimal(i) for i in sizestz))
 percentsizestz= (arrysizestz * 100) /totaltz
-#pichart for timezone
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = labelstz
 sizes = percentsizestz
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-    shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-plt.savefig(pp, format='pdf')
+figure(figsize=(8,8))
+ax = axes([0.1, 0.1, 0.8, 0.8])
+pie(sizes, labels=labels,
+                autopct='%1.1f%%', shadow=True, startangle=90)
+title('Timezone', bbox={'facecolor':'0.8', 'pad':5})
+savefig(pp, format='pdf')
 
 
 #time
-timeresult = Counter(time)
-plottime = timeresult.items()
-labelstime, sizestime = zip(*plottime)
-arrysizestime= numpy.array(sizestime)
-totaltime=numpy.array(sum(Decimal(i) for i in sizestime))
-cumsizestime= np.cumsum(arrysizestime)
-#scatter chart for time
-
-plt.savefig(pp, format='pdf')
+# timeresult = Counter(time)
+# plottime = timeresult.items()
+# labelstime, sizestime = zip(*plottime)
+# arrysizestime= np.array(sizestime)
+# totaltime=np.array(sum(Decimal(i) for i in sizestime))
+# cumsizestime= np.cumsum(arrysizestime)
+# scatter(labelstime, cumsizestime, s=20, c='b', marker='o', cmap=None, norm=None,
+#         vmin=None, vmax=None, alpha=None, linewidths=None,
+#         verts=None, **kwargs)
+# #scatter chart for time
+#
+# plt.savefig(pp, format='pdf')
 
 #hashtags
 hshresult = Counter(allhash)
+plothsh = hshresult.items()
 labelshsh, sizeshsh = zip(*plothsh)
-arrysizeshsh= numpy.array(sizeshsh)
-totalhsh=numpy.array(sum(Decimal(i) for i in sizeshsh))
+arrysizeshsh= np.array(sizeshsh)
+totalhsh=np.array(sum(Decimal(i) for i in sizeshsh))
 percentsizeshsh= (arrysizeshsh * 100) /totalhsh
 #pichart for timezone
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = labelshsh
 sizes = percentsizeshsh
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-    shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-plt.savefig(pp, format='pdf')
+figure(figsize=(8,8))
+ax = axes([0.1, 0.1, 0.8, 0.8])
+pie(sizes, labels=labels,
+                autopct='%1.1f%%', shadow=True, startangle=90)
+                 # Equal aspect ratio ensures that pie is drawn as a circle.
+title('Hashtags', bbox={'facecolor':'0.8', 'pad':5})
+savefig(pp, format='pdf')
 
 #language
 langresult = Counter(language)
+plotlang = langresult.items()
 labelslang, sizeslang = zip(*plotlang)
-arrysizeslang= numpy.array(sizeslang)
-totallang=numpy.array(sum(Decimal(i) for i in sizeslang))
+arrysizeslang= np.array(sizeslang)
+totallang=np.array(sum(Decimal(i) for i in sizeslang))
 percentsizeslang= (arrysizeslang * 100) /totallang
 #pichart for timezone
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = labelslang
 sizes = percentsizeslang
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-    shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-plt.savefig(pp, format='pdf')
-
+figure(figsize=(8,8))
+ax = axes([0.1, 0.1, 0.8, 0.8])
+pie(sizes, labels=labels,
+                autopct='%1.1f%%', shadow=True, startangle=90)
+title('Language', bbox={'facecolor':'0.8', 'pad':5})
+savefig(pp, format='pdf')
 pp.close()
